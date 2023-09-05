8e88bdc11d5286f23d6081727fc6fd47843de894
Duh0127 <rodatupmocgamer22@gmail.com>
2023-09-05 19:21:01 -0300

    ajustando melhorias no componente MENU

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Menu(){
     return(
         <nav className="menu">
-            <Link to="/">Home</Link>
-            <span> | </span>
-            <Link to="/produtos">Produtos</Link>
+            <Link to="/">HOME</Link>
+            <span> - </span>
+            <Link to="/produtos">PRODUTOS</Link>
         </nav>
     )
 }
