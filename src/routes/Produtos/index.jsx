238513f126c87d909349ecec7bfdd8852be16a79
Duh0127 <rodatupmocgamer22@gmail.com>
2023-09-05 19:21:58 -0300

    Alterações na página de produtos

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Produtos(){
     return(
         <main>
-            <h1>Produtos</h1>
+            <h1>MINHA LISTA DE PRODUTOS</h1>
             {listaProdutos.map(prod => (
                 <div key={prod.id}>
                     <Link to={`/produtos/editar/${prod.id}`}>
